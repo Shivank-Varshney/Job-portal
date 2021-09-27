c91ebee494288cb5e90d8e829dcba4408cefce6b
SHIVANK VARSHNEY <74199623+Shivank-Varshney@users.noreply.github.com>
2021-09-27 10:45:11 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { JobDescComponent } from './job-desc/job-desc.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    JobDescComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
